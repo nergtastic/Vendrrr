0db4c0766c142776028ea258c59b598217b1aac7
nergtastic <nergon@gmail.com>
2018-07-30 20:47:47 -0600

    Encapsulated code

--- vender-controller.js
+++ vender-controller.js
@@ -1,20 +1,107 @@
 function VenderController(){
   //private parts
-  var venderService = new VenderService()
+  let venderService = new VenderService()
 
   //items is an array we need to get from the service and give to drawItems
-  function drawItems(items){
-    //we will take in a paramter(items) and iterate over it to build
-    //a template to draw to the screen.
+  // function drawItems(items){
+  //   //we will take in a paramter(items) and iterate over it to build
+  //   //a template to draw to the screen.
+  //     let machineElem = document.getElementById('items')
+  //     let template = ""
+  //     console.log(items.length)
+  //     for(let i = 0; i < items.length; i++){
+  //       let item = items[i]
+  //       console.log(item)
+  //       console.log(item.name)
+  //       console.log(item.description)
+  //       console.log(item.price)
+  //       template += `
+  //       <div class='item-container'>
+  //       <h3>${item.name}</h3>
+  //       <h5>${item.description}</h5>
+  //       <h5>${item.price}</h5>
+  //       </div>
+  //       `
+  //     }
+  //     machineElem.innerHTML = template   
+  // }
 
+//   function drawMoney(){
+//     let machineElem = document.getElementById('moneyButton')
+//     let template = 
+//     `
+//     <button onclick='venderService.addMoney()'>Add Quarter</button>
+//     `
+// }
+
+function drawItems(items){
+  let money = venderService.getMoney()
+  let template = ""
+  for(index = 0; index < items.length; index++){
+    const item = items[index];
+
+    if(money < item.price){
+      template += `
+      <div style="outline: 1px solid red; border-radius: 5px; padding: 25px; margin: 25px;" class="col-3">
+      <h3>${item.name}</h3>
+      <p>${item.description}</p>
+      <p>$ ${item.price}</p>
+      <p>Qty: ${item.amount}</p>
+      <button disabled onclick="app.controllers.venderController.purchase(${index})">Purchase</button>
+      </div>
+      `
+    } else {
+      template += `
+      <div style="outline: 1px solid red; border-radius: 5px; padding: 25px; margin: 25px;" class="col-3">
+      <h3>${item.name}</h3>
+      <p>${item.description}</p>
+      <p>$ ${item.price}</p>
+      <p>Qty: ${item.amount}</p>
+      <button onclick="app.controllers.venderController.purchase(${index})">Purchase</button>
+      </div>
+      `
+    }
+      document.getElementById('items').innerHTML = template 
+      document.getElementById('money').innerHTML = money
   }
+}
 
+function drawMoney(){
+  document.getElementById('money').innerText = venderService.getMoney()
+}
+
+function drawPurchased(item){
+  let template = `
+  <h3 style="margin-top: 50px;">Last Purchased Item:</h3>
+  <h3>${item.name}</h3>
+  <p>${item.description}</p>
+  <p>$ ${item.price}</p>
+  `
+  document.getElementById('purchased').innerHTML = template
+}
 
   //public parts
 
+this.purchase = function(index){
+  // venderService.purchase(index)
+  // drawMoney()
+  let item = venderService.purchase(index)
+  drawItems(venderService.getItems())
+  if (item){
+    drawPurchased(item)
+  }
+}
+
+this.addMoney = function(){
+  venderService.addMoney()
+  drawItems(venderService.getItems())
+}
+
+
   
   //we need a function to take money from our "view" and pass it to our service
-
+drawMoney()
+drawItems(venderService.getItems())
 
 
 }
--- vender-service.js
+++ vender-service.js
@@ -1,44 +1,85 @@
 function VenderService(){
   //private parts
-  var money = 0
+  let money = 0
 
   //our purchaseable items, add some of your own!
   //we could add an id property to these items to display location for purchasing(A1, D4, etc)
-  var items = [
+  let items = [
     {
       name: "Fruit Snack Gummies",
       description: "Fruit salad done right.",
       price: 1.00,
-      amount: 45
+      amount: 45,
+      location: 'A1'
     },
     {
       name: "Plant Food",
       description: "Are you hungry? I could use a light snack.",
       price: 5.00,
-      amount: 20
+      amount: 20,
+      location: 'A2'
     },
     {
       name: "Admiral-able Ack-Bar",
       description: "Snack like an Admiral.",
       price: .75,
-      amount: 3
+      amount: 3,
+      location: 'A3'
     },
     {
       name: "Paranormal Snacktivity",
       description: "The perfect midnight snack.",
       price: .25,
-      amount: 7
+      amount: 7,
+      location: 'B1'
     },
     {
       name: "The Void",
       description: "This snack eats you!",
       price: 3.25,
-      amount: 1
+      amount: 1,
+      location: 'B2'
     }
   ]
 
   //public parts
+  this.getItems = function(){
+    let itemsCopy = []
+    
+    for (let i = 0; i < items.length; i++){
+      const item = items[i];
+      let itemCopy = {
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        amount: item.amount
+      }
+      itemsCopy.push(itemCopy)
+    }
+    return itemsCopy
+  }
 
+  this.purchase = function(index){
+    let item = items[index]
+    if(money >= item.price && item.amount > 0){
+      item.amount--
+
+      money -= item.price
+
+      console.log('purchased ', item.name)
+      return{
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        amount: item.amount
+      }
+    }
+
+  }
+
+  this.getMoney = function(){
+    return money
+  }
   
   this.addMoney = function(){
     money += .25
